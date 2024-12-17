95833316235332af03f48f741b2edf12ee4fcf08
Soley-Man <59509876+Soley-Man@users.noreply.github.com>
2024-12-17 16:07:13 -0500

    Turn the code into a function to scrape wiki pages
    Place the existing code inside a function. The function is called on a url linking a wikipedia page. Then it randomly selects from that page a link to another wiki page, and calls itself on the new url. Thus it scrapes wikipedia endlessly.

--- interesting-wiki-scraper.py
+++ interesting-wiki-scraper.py
@@ -4,28 +4,34 @@ import random
 
 starting_url = "https://en.wikipedia.org/wiki/Web_scraping"
 
-source = requests.get(url = starting_url)
-soup = BeautifulSoup(source.content, "html.parser")
+def crawlWikiPage(url):
+    source = requests.get(url = url)
+    soup = BeautifulSoup(source.content, "html.parser")
 
-title = soup.find(id = "firstHeading")
-print(title.text)
+    title = soup.find(id = "firstHeading")
+    print(title.text)
 
-allLinks = soup.find(id = "bodyContent").find_all("a")
-random.shuffle(allLinks)
+    allLinks = soup.find(id = "bodyContent").find_all("a")
+    random.shuffle(allLinks)
 
-for link in allLinks:
-    # Only follow links that lead to another wiki page:
-    if link["href"].startswith("/wiki/"):
-        # Exclude namespaces like Special:
-        if ":" in link["href"]:
-            continue
+    for link in allLinks:
+        # Only follow links that lead to another wiki page:
+        if link["href"].startswith("/wiki/"):
+            # Exclude namespaces like Special:
+            if ":" in link["href"]:
+                continue
 
-        # Exclude links in the reference list
-        if link.find_parent(class_ = "reflist"):
-            continue
+            # Exclude links in the reference list
+            if link.find_parent(class_ = "reflist"):
+                continue
 
-        # Exclude links in a <table> element
-        if link.find_parent("table"):
-            continue
+            # Exclude links in a <table> element
+            if link.find_parent("table"):
+                continue
 
-        print(link["href"])
+            new_url = link["href"]
+            break
+    
+    crawlWikiPage("https://en.wikipedia.org" + new_url)
+
+crawlWikiPage(starting_url)

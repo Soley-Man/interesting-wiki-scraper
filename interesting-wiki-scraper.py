5e43f0cfafd18b1ecaf63d43c26156baa98b7fba
Soley-Man <59509876+Soley-Man@users.noreply.github.com>
2024-12-17 15:47:14 -0500

    Add spaces around equal sign on line 10

--- interesting-wiki-scraper.py
+++ interesting-wiki-scraper.py
@@ -7,7 +7,7 @@ starting_url = "https://en.wikipedia.org/wiki/Web_scraping"
 source = requests.get(url = starting_url)
 soup = BeautifulSoup(source.content, "html.parser")
 
-title = soup.find(id="firstHeading")
+title = soup.find(id = "firstHeading")
 print(title.text)
 
 allLinks = soup.find(id = "bodyContent").find_all("a")
